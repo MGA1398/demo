0f886a30f14de0ca248c0986e1cb56922e99df27
mga1398 <martingalvez2@gmail.com>
2019-08-26 10:52:42 -0500

    doctorisy

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    XyzComponent
+  ],
+  imports: [
+    // NgbModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
